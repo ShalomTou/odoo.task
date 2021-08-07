d006190a0d39c33c73cc5528108b0b5d8f27af90
ShaloTou <interamrez92@gmail.com>
2021-08-07 11:38:48 +0300

    add ReadMe and fixing issues

---
+++ part3/problem3/src/main.js
@@ -0,0 +1,8 @@
+// main.js ---------------------------------------------------------------------
+import { App } from "/app.js";
+
+function setup() {
+  mount(App, { target: document.body });
+}
+
+owl.utils.whenReady(setup);
